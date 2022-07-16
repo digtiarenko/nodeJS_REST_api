49fd4c09fa1815c689b2151c6ee0b0827745d892
Valentyn Digtiarenko <baggins999@gmail.com>
2022-07-16 21:15:05 +0300

    first commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,103 @@
-const express = require('express')
+const express = require('express');
+const { createError } = require('../../helpers');
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../../models/contacts');
+const Joi = require('joi');
 
-const router = express.Router()
+const router = express.Router();
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().max(50).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required(),
+  phone: Joi.string()
+    .pattern(/^[0-9+()-_ ]*$/)
+    .max(20)
+    .required(),
+});
+const putSchema = Joi.object({
+  name: Joi.string().max(50),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ['com', 'net'] },
+  }),
+  phone: Joi.string()
+    .pattern(/^[0-9+()-_ ]*$/)
+    .max(20),
+});
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const result = await listContacts();
+    console.log(result);
+    res.json(result).status(200);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const result = await getContactById(contactId);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json(result).status(200);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+
+    const result = await addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { contactId } = req.params;
+    const result = await removeContact(contactId);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json({ message: 'Contact deleted' });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { error } = putSchema.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const { contactId } = req.params;
+    console.log('contactId', contactId);
+    const result = await updateContact(contactId, req.body);
+    if (!result) {
+      throw createError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
