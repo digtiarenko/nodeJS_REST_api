d8d5b98e5355f4b090016769402b5590c98775e7
Valentyn Digtiarenko <baggins999@gmail.com>
2022-12-20 19:26:08 +0200

    connecting APIs

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -3,8 +3,9 @@ const { createError } = require('../../helpers');
 
 const getById = async (req, res, next) => {
   const { contactId } = req.params;
+  const { _id } = req.user;
   try {
-    const result = await services.getOneContact(contactId);
+    const result = await services.getOneContact(contactId, _id);
     if (!result) {
       throw createError(404);
     }
--- services/contacts/getOneContact.js
+++ services/contacts/getOneContact.js
@@ -1,7 +1,26 @@
 const Contact = require('../../models/contact.js');
 
-const getOneContact = async contactId => {
-  const data = await Contact.findById(contactId);
+// const getOneContact = async (contactId, _id) => {
+//   const data = await Contact.find({
+//     $and: [{ _id: contactId }, { owner: _id }],
+//   });
+//   return data;
+// };
+
+const getOneContact = async (contactId, _id) => {
+  const data = await Contact.find({
+    $and: [
+      {
+        _id: { $eq: contactId },
+        owner: { $eq: _id },
+      },
+    ],
+  });
   return data;
 };
 module.exports = getOneContact;
+
+// db.bios.find({
+//   birth: { $gt: new Date('1920-01-01') },
+//   death: { $exists: false },
+// });
