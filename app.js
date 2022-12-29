da0b207718703d1f1ee990170defcc7d43c40702
Valentyn Digtiarenko <baggins999@gmail.com>
2022-12-29 21:33:22 +0200

    fixing cors issues

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://marvelous-bunny-daf51a.netlify.app/',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.static('public'));
 
