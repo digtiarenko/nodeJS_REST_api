26e4a2ca86705cc15871eb7f939bfbdfaf7b699d
Valentyn Digtiarenko <baggins999@gmail.com>
2022-12-29 22:52:27 +0200

    **

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 const authRouter = require('./routes/api/auth');
 const contactsRouter = require('./routes/api/contacts');
@@ -9,8 +9,28 @@ const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
-app.options('*', cors());
-app.use(cors());
+
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    'http://localhost:3004',
+    'http://gamebrag.onrender.com',
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.header('Access-Control-Allow-credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
+  next();
+});
+
+// app.options('*', cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
