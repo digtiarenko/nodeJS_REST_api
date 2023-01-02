84a4593e737481fd4694b9f7340e64803d751ce1
Valentyn Digtiarenko <baggins999@gmail.com>
2023-01-02 12:39:25 +0200

    setting ne hosting

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 const express = require('express');
 const logger = require('morgan');
-// const cors = require('cors');
+const cors = require('cors');
 require('dotenv').config();
 const authRouter = require('./routes/api/auth');
 const contactsRouter = require('./routes/api/contacts');
 
 const app = express();
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    'http://localhost:3004',
-    'https://marvelous-bunny-daf51a.netlify.app/',
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
-  res.header('Access-Control-Allow-credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
-  next();
-});
+app.options('*', cors());
+app.use(cors());
 
-// app.options('*', cors());
-// app.use(cors());
+// app.use(function (req, res, next) {
+//   // res.header("Access-Control-Allow-Origin", "*");
+//   const allowedOrigins = ['https://phonebook-back.onrender.com'];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+//   );
+//   res.header('Access-Control-Allow-credentials', true);
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
+//   next();
+// });
+
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/contacts', contactsRouter);
 app.use('/users', authRouter);
+app.use('/contacts', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
